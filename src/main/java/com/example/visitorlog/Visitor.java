b381fe1e177fdaa49b1ed59721bd1bda33759767
Hajar <hajar2000alfarsi@gmail.com>
2026-07-15 09:09:28 +0400

    implement fields for visttor with getters and setters, and constructor

---
+++ src/main/java/com/example/visitorlog/Visitor.java
@@ -0,0 +1,18 @@
+package com.example.visitorlog;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visitor {
+    private Long id;
+
+    private String name;
+
+    private String company;
+
+    private String purpose;
+}
